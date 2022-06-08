c10fed92166d3a35a3dddaa2d4167cb6c3479ff2
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-06-08 16:57:32 -0700

    Done

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary.update({key: value})
+        return a_dictionary
     else:
         a_dictionary.update({key: value})
-    for i in a_dictionary:
-        print("{:s}: {}".format(i, a_dictionary[i]))
+        return a_dictionary
