8e3cd9dd7f25500e5fe8e3f2919ec5e1ef129be6
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-06-20 12:52:28 -0700

    Done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         isinstance(value, int)
-        print("{:d}".format(value))
+        print("{:d}".format(value), end="\n")
         return True
-    except TypeError:
+    except:
         return False
