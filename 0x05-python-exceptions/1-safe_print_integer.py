b6ea37c6e299f7a6ec5b67987e399f676667688e
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-06-20 12:26:16 -0700

    Done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     try:
         x = int(value / 1)
-        print(x)
+        print("{:d}".format(x))
         return True
     except TypeError:
         return False
